40587a34646916c004ab46d84dc69d22fc23dd6b
mubiinaafaaris <mubiinaafaaris@gmail.com>
2026-02-20 11:42:07 +0300

    html

---
+++ basic programming/script.js
@@ -0,0 +1,17 @@
+console.log("Programming koo hojii jalqabeera");
+
+let maqaa = "M";
+let umurii = 10; // umurii sirrii barreessi
+
+let biyyakoo = "J";
+
+console.log("Ani maqaankoo: ", maqaa, "dha. ", "Umuriinkoo: ", umurii, " lafan itti dhaladhe: ", biyyakoo, " dha.");
+
+// calculation
+console.log('calculation \n\n')
+
+let umuriikeewaggaadhufuu = umurii + 1;  // waggaa dhufu
+let umuriikonkolaachisuu = umurii - 18; // konkolaachisuu eegaluuf waggaa
+
+console.log("Waggaa dhufu umuriin kee ", umuriikeewaggaadhufuu, " ta'a!");
+console.log("Konkolaachisuu eegaluuf waggaa", umuriikonkolaachisuu, " tu siif hafa");
